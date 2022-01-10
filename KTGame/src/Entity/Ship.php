4ae2fffc5e1a02be899b12e091ba88c4e42f26e3
Kerlan PLUMASSEAU <kerlan.plumasseau@epitech.eu>
2022-01-10 03:01:14 +0100

    Entity create

---
+++ KTGame/src/Entity/Ship.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ShipRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ShipRepository::class)]
+class Ship
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'integer')]
+    private $quantity;
+
+    #[ORM\Column(type: 'integer')]
+    private $timestamp;
+
+    #[ORM\Column(type: 'integer')]
+    private $level;
+
+    #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'ships')]
+    #[ORM\JoinColumn(nullable: false)]
+    private $user;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity): self
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    public function getTimestamp(): ?int
+    {
+        return $this->timestamp;
+    }
+
+    public function setTimestamp(int $timestamp): self
+    {
+        $this->timestamp = $timestamp;
+
+        return $this;
+    }
+
+    public function getLevel(): ?int
+    {
+        return $this->level;
+    }
+
+    public function setLevel(int $level): self
+    {
+        $this->level = $level;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
---
+++ KTGame/src/Services/Soldier.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+class Soldier extends Attack {
+    public static int $globalQuantity = 0;
+    private const COEF = 1.5;
+    private int $price = 10;
+    private int $buildingTime = 120;
+
+    public function __construct(private int $lvl = 1)
+    {
+        $this->lvl = $lvl;
+        $this->price *= $lvl * COEF;
+        $this->buildingTime *= $lvl * COEF;
+        $this->pv = 1000 * $lvl * COEF;
+        $this->dps = 100 * $lvl * COEF;
+    }
+
+    public function setLvl(int $lvl)
+    {
+        $this->lvl = $lvl;
+    }
+
+    public function getLvl(): int
+    {
+        return $this->lvl;
+    }
+
+    public function setPrice(int $price)
+    {
+        $this->price = $price;
+    }
+
+    public function getPrice(): int
+    {
+        return $this->price;
+    }
+
+    public function setBuildingTime(int $buildingTime)
+    {
+        $this->buildingTime = $buildingTime;
+    }
+
+    public function getBuildingTime(): int
+    {
+        return $this->buildingTime;
+    }
+
+}
