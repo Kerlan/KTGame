ca6ff3edb9d9875d72d4d86839d6f99ffb5b95f0
X_ROCKEFELLER_X <tommy.simeon@hotmail.fr>
2022-01-11 00:12:14 +0100

    home controller

--- KTGame/src/Controller/HomeController.php
+++ KTGame/src/Controller/HomeController.php
@@ -6,12 +6,18 @@ use App\Services\Gold;
 use App\Services\Diamond;
 use App\Services\Metal;
 
+// use App\Entity\Metal;
+// use App\Entity\Diamond;
+// use App\Entity\Gold;
+
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+
     public int $goldStock;
     public int $metalStock;
     public int $diamondStock;
@@ -24,13 +30,10 @@ class HomeController extends AbstractController
 
     public function takeFixData()
     {
-        $this->goldStock = 14;
+        $this->goldStock;
         $this->metalStock = 7;
         $this->diamondStock = 2;
-        $this->shipStock = 21;
-        $this->unityStock = 34;
     }
-
 
     public function takeEnnemies()
     {
@@ -69,26 +72,75 @@ class HomeController extends AbstractController
         );
     }
 
+    // public function findSomethingByUser(User $user)
+    // {
+    //     $query = $this->getEntityManager()
+    //         ->createQuery(
+    //             'SELECT t FROM AppBundle:Table t '.
+    //             'WHERE t.user > :user'
+    //     )->setParameter('user', $user->getUsername());
+
+    //     return $query->getResult();
+    // }
+
+    public function calcStockGold()
+    {
+        $this->goldStock = 0;
+
+        for ($i = 0; $i != count($goldBuildings); $i++) {
+            $this->goldStock += $goldBuildings[i]->stock;
+        }
+        return $this->goldStock;
+    }
+
+    public function calcStockMetal()
+    {
+        $metalStock = 0;
+
+        for ($i = 0; $i != count($metalBuildings); $i++) {
+            $metalStock += $metalBuildings[i]->stock;
+        }
+        return $metalStock;
+    }
+
+    public function calcStockDiamond()
+    {
+        $diamondBuildings = 0;
+
+        for ($i = 0; $i != count($diamondBuildings); $i++) {
+            $diamondBuildings += $diamondBuildings[i]->stock;
+        }
+        return $diamondBuildings;
+    }
 
     #[Route('/home', name: 'home')]
-    public function index(): Response
+    public function index($email): Response
     {
+        $user = $this->getDoctrine()->getRepository('AppBundle:')->find($email);
         $this->takeFixData();
-        $this->takeEnnemies();
-        $this->takeGoldBuildings();
-        $this->takeMetalBuildings();
-        $this->takeDiamondBuildings();
+        $this->calcStockGold();
+        $this->calcStockMetal();
+        $this->calcStockDiamond();
+
+        $shipStock = $user->getShips();
+        $unityStock = $user->getSoldiers();
+        $ennemies = $user->ennemies;
+        $goldBuildings = $user->getGolds();
+        $metalBuildings = $user->getMetals();
+        $diamondBuildings = $user->getDiamonds();
+
+       // $this->takeGoldBuildings();
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-            'goldStock' => $this->goldStock,
-            'metalStock' => $this->metalStock,
-            'diamondStock' => $this->diamondStock,
-            'shipStock' => $this->shipStock,
-            'unityStock' => $this->unityStock,
-            'ennemies' => $this->ennemies,
-            'goldBuildings' => $this->goldBuildings,
-            'metalBuildings' => $this->metalBuildings,
-            'diamondBuildings' => $this->diamondBuildings,
+            'goldStock' => $user->goldStock,
+            'metalStock' => $user->metalStock,
+            'diamondStock' => $user->diamondStock,
+            'shipStock' => $shipStock,
+            'unityStock' => $unityStock,
+            'ennemies' => $user->ennemies,
+            'goldBuildings' => $goldBuildings,
+            'metalBuildings' => $metalBuildings,
+            'diamondBuildings' => $diamondBuildings,
 
         ]);
     }
